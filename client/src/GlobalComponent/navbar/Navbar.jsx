b9a1ba16c38c0a86c9230ebf1ada285183e908aa
morshedulmunna <morshedulmunna1@gmail.com>
2022-03-24 16:04:04 +0600

    Dropdown Added

--- client/src/GlobalComponent/navbar/Navbar.jsx
+++ client/src/GlobalComponent/navbar/Navbar.jsx
@@ -1,31 +1,33 @@
 import "./Navbar.scss";
 import { AiOutlineSearch } from "react-icons/ai";
-import { FaChessQueen } from "react-icons/fa";
+import { FaChessQueen, FaLongArrowAltRight } from "react-icons/fa";
 import { GiChaliceDrops } from "react-icons/gi";
 
 const Navbar = () => {
   return (
     <div className="Navbar ">
       <div className="nav container">
-        <div className="search_area">
+        <div className="search-logo">
           <img
             src="https://www.uplabs.com/logos/uplabs/default--color.svg"
             alt="Logo"
           />
-          <div className="input">
-            <input type="search" placeholder="Search" />
-            <span id="search-icons">
-              <AiOutlineSearch />
-            </span>
+          <div className="search_area">
+            <div className="input">
+              <input type="search" placeholder="Search" />
+              <span id="search-icons">
+                <AiOutlineSearch />
+              </span>
+            </div>
+            <span>GET PREMIUM</span>
           </div>
-          <span>GET PREMIUM</span>
         </div>
         <div className="right_area">
           <div className="down-Chall">
             <span className="Downloads-Btn">
               <FaChessQueen /> Get Unlimited Downloads
             </span>
-            <span>
+            <span className="challenges">
               <GiChaliceDrops /> Challenges
             </span>
           </div>
@@ -37,13 +39,100 @@ const Navbar = () => {
       </div>
       <div className=" container nav-btn">
         <span>Trending</span>
-        <span>Templates</span>
+        <div class="dropdown">
+          <span>Templates</span>
+          <div class="dropdown-content">
+            <div className="dropdown-menu">
+              <div className="dropdown-left">
+                <span>Web Templates</span>
+                <span>Andriod Templates</span>
+                <span>Logo Templates</span>
+                <span>Business Card Templates</span>
+                <span>Presentation Templates</span>
+                <span>Infographic Templates</span>
+                <span>Email Templates</span>
+                <span id="all-temp">
+                  All Templates <FaLongArrowAltRight />
+                </span>
+              </div>
+              <div className="dropdown-right">
+                <span>iOS Templates</span>
+                <span>Others Templates</span>
+                <span>Social Media Templates</span>
+                <span>Resume Templates</span>
+                <span>Cerfiticate Templates</span>
+              </div>
+            </div>
+          </div>
+        </div>
         <span>Illustrations</span>
-        <span>UI kits</span>
+        {/* Drop Down ====> */}
+        <div class="dropdown">
+          <span>Ui Kits</span>
+          <div class="dropdown-content">
+            <div className="dropdown-menu">
+              <div className="dropdown-left">
+                <span>iOS UI kits</span>
+                <span>Android UI kits</span>
+                <span id="all-temp">
+                  All Ui Kits
+                  <FaLongArrowAltRight />
+                </span>
+              </div>
+              <div className="dropdown-right">
+                <span>Web UI Kits</span>
+                <span>Others UI Kits</span>
+              </div>
+            </div>
+          </div>
+        </div>
+        {/* Drop Down ====> */}
+
         <span>Icons</span>
-        <span>Mockups</span>
+        {/* Drop Down ====> */}
+        <div class="dropdown">
+          <span>Mockups</span>
+          <div class="dropdown-content">
+            <div className="dropdown-menu">
+              <div className="dropdown-left">
+                <span>iOS Mockups</span>
+                <span>Android Mockups</span>
+                <span id="all-temp">
+                  All Mockups
+                  <FaLongArrowAltRight />
+                </span>
+              </div>
+              <div className="dropdown-right">
+                <span>Others Mockups</span>
+                <span>Android Mockups</span>
+              </div>
+            </div>
+          </div>
+        </div>
+        {/* Drop Down ====> */}
         <span>Themes</span>
-        <span>Fonts</span>
+        {/* Drop Down ====> */}
+        <div class="dropdown">
+          <span>Fonts</span>
+          <div class="dropdown-content">
+            <div className="dropdown-menu">
+              <div className="dropdown-left">
+                <span>Others Fonts</span>
+                <span>Scripts Fonts</span>
+                <span>Serif Fonts</span>
+                <span id="all-temp">
+                  All Fonts
+                  <FaLongArrowAltRight />
+                </span>
+              </div>
+              <div className="dropdown-right">
+                <span>Decorative Fonts</span>
+                <span>Sans-Serif Fonts</span>
+              </div>
+            </div>
+          </div>
+        </div>
+        {/* Drop Down ====> */}
       </div>
     </div>
   );
