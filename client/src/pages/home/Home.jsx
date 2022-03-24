03ad65ce23e665ef32d35029fe6b37cfcb4bfaef
morshedulmunna <morshedulmunna1@gmail.com>
2022-03-24 17:14:44 +0600

    trading Design File

--- client/src/pages/home/Home.jsx
+++ client/src/pages/home/Home.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import HomeHeader from "./homeComponent/HomeHeader";
+import TrendingDesign from "./trendingDesign/TrendingDesign";
 
 const Home = () => {
   return (
     <div>
       <HomeHeader />
+      <TrendingDesign />
     </div>
   );
 };
---
+++ client/src/pages/home/trendingDesign/TrendingDesign.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const TrendingDesign = () => {
+  return (
+    <div className="container">
+      <h2>Trending Design File</h2>
+    </div>
+  );
+};
+
+export default TrendingDesign;
